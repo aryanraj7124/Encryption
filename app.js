77463a6d6aa92e76fb8831de8af5e022a450f1b2
aryanraj7124 <aryanraj712400@gmail.com>
2024-12-22 16:58:51 +0530

    md5

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 app.use(express.static("public"));
 
@@ -33,7 +34,7 @@ const User = new mongoose.model("User", userSchema);
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser
@@ -49,7 +50,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", async function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   try {
     const foundUser = await User.findOne({ email: username });
